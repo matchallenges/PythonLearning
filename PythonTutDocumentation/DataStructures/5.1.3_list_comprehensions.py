3992316096bc052d86297d92e8f52530a6187dde
matchallenges <matchallenges04@gmail.com>
2020-06-30 17:56:14 -0400

    Filter

--- PythonTutDocumentation/DataStructures/5.1.3_list_comprehensions.py
+++ PythonTutDocumentation/DataStructures/5.1.3_list_comprehensions.py
@@ -3,7 +3,7 @@
 squares = []
 for x in range(10):
      squares.append(x**2)
-#print (squares) # this is how we create a list normally with squares
+print (squares) # this is how we create a list normally with squares
 
 # or we can do a list comprehesion
 
@@ -21,9 +21,15 @@ multiply = [x*2 for x in squares] # in list comprehensions you can you use for a
 
 triplets = [[x*2, y*4, z*6] for x in range(10) for y in range(10) for z in range(10) if x**3 == y]
 
-print (triplets) # prints triplets ten times each and searches for when x cubed is equal to y
+#print (triplets) # prints triplets ten times each and searches for when x cubed is equal to y
 
 #print([x*2 for x in squares]) # the comprehesion can automatically create a new list without being assigned to a variable 
 
 # this list doubles the squares in the first list
+
+print([x for x in squares if x < 0]) # will filter positive of negative numbers
+
+# apply a function to all the elements
+
+
 
