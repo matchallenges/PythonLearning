3f90e36f3d2e0d1084590fabc43d62b42eef77a9
matchallenges <matchallenges04@gmail.com>
2020-06-29 21:30:03 -0400

    Define positional args

---
+++ PythonTutDocumentation/FlowControlTools/keyword_arguments_vs_positional.py
@@ -0,0 +1,7 @@
+def positional(a, b):
+    print ("the first arg is", a, "the second arg is", b)
+
+positional(1, 2)
+positional(2, 1)
+
+#positional arguments are based on the position that you pass the value to the function a b or b a
