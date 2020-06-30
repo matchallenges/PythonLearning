b170c87bb693ed2db3d3d0d77dd1fbe5e72ef7c8
matchallenges <matchallenges04@gmail.com>
2020-06-30 17:54:05 -0400

    List comprehension 2

--- PythonTutDocumentation/DataStructures/5.1.3_list_comprehensions.py
+++ PythonTutDocumentation/DataStructures/5.1.3_list_comprehensions.py
@@ -3,7 +3,7 @@
 squares = []
 for x in range(10):
      squares.append(x**2)
-print (squares) # this is how we create a list normally with squares
+#print (squares) # this is how we create a list normally with squares
 
 # or we can do a list comprehesion
 
@@ -11,8 +11,19 @@ squares = [x**2 for x in range(20)] # the x is the operation you want to do
 
 # and then you can add flow control statements pertaining to that operation
 
-print (squares)
+#print (squares)
 
-multiply = [x*2 ] # in list comprehensions you can you use for and if clauses
+multiply = [x*2 for x in squares] # in list comprehensions you can you use for and if clauses
 
-print (multiply)
+#print (multiply)
+
+# A list comprehension consists of brackets containing an expression followed by a for clause, then zero or more for or if clauses.
+
+triplets = [[x*2, y*4, z*6] for x in range(10) for y in range(10) for z in range(10) if x**3 == y]
+
+print (triplets) # prints triplets ten times each and searches for when x cubed is equal to y
+
+#print([x*2 for x in squares]) # the comprehesion can automatically create a new list without being assigned to a variable 
+
+# this list doubles the squares in the first list
+
