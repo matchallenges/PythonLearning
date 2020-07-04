f69fa450c066823f90488ad6f0fdf0674049aae5
matchallenges <matchallenges04@gmail.com>
2020-07-04 19:14:20 -0400

    Rules

---
+++ PythonTutDocumentation/InputAndOutput/format().py
@@ -0,0 +1,10 @@
+""" "First, thou shalt count to {0}"  # References first positional argument
+"Bring me a {}"                   # Implicitly references the first positional argument
+"From {} to {}"                   # Same as "From {0} to {1}"
+"My quest is {name}"              # References keyword argument 'name'
+"Weight in tons {0.weight}"       # 'weight' attribute of first positional arg
+"Units destroyed: {players[0]}"   # First element of keyword argument 'players'."""
+
+""" "Harold's a clever {0!s}"        # Calls str() on the argument first
+"Bring out the holy {name!r}"    # Calls repr() on the argument first
+"More {!a}"                      # Calls ascii() on the argument first """
