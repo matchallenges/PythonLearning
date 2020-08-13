bf1407c58bf341e848da3534fc93433c7dd425dc
matchallenges <matchallenges04@gmail.com>
2020-08-13 15:12:17 -0400

    listening for inbound connections

--- PythonVideoTutorials/server.py
+++ PythonVideoTutorials/server.py
@@ -1,3 +1,9 @@
 import socket # std library
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((socket.gethostname(), 1234))
+s.listen(5)
+
+while True:
+    clientsocket, adress = s.accept()
+    print(f"Server had connected to {adress} successfully!")
