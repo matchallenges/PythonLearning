6d8cca4ea00f794344deae8c5e2c4a6ab30ce47d
matchallenges <matchallenges04@gmail.com>
2020-08-16 23:43:53 -0400

    peer 1 and 2

--- PythonVideoTutorials/client.py
+++
@@ -1,8 +0,0 @@
-import socket # std library
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 5000))
-
-while True:
-    msg = s.recv(8)
-    print(msg.decode())
---
+++ PythonVideoTutorials/fomat.py
@@ -0,0 +1,4 @@
+#msg = '{:>20}'.format('Welcome to the server')
+#print(msg)
+int = 5
+print(f'{int:>10}')
