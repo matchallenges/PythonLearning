a31fb0dc179f4c123af4fed8d19b8e8e8efb7658
matchallenges <matchallenges04@gmail.com>
2020-08-17 18:13:45 -0400

    Finished peer to peer chat

--- PythonVideoTutorials/fomat.py
+++ PythonVideoTutorials/fomat.py
@@ -2,3 +2,30 @@
 #print(msg)
 int = 5
 print(f'{int:>10}')
+
+    """
+    msg = "Welcome to the server!"
+    header_with_msg = f'{len(msg):<{HEADERSIZE}}' + msg
+    
+    clientsocket.send(bytes(header_with_msg, "utf-8"))
+    """
+
+#client side logic
+"""
+while True:
+    decoded_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(50)
+        if new_msg:
+            print("Incomming message length: ", msg[:HEADERSIZE])
+            message_length = int(msg[:HEADERSIZE])
+            new_msg = False
+
+        decoded_msg += msg.decode("utf-8")
+        
+        if len(decoded_msg)-HEADERSIZE == message_length:
+            print("Full Message received")
+            print(decoded_msg[HEADERSIZE:])
+            new_msg = True
+"""
--- PythonVideoTutorials/peer1.py
+++ PythonVideoTutorials/peer1.py
@@ -1,43 +1,36 @@
-import socket #std library
+import socket
+import threading
 import time
 
 sA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HEADERSIZE = 10
-
 #binding socket
-sA.bind((socket.gethostname(), 5000)) #bind to a port
+sA.bind((socket.gethostname(), 9000)) #bind to a port
 sA.listen(5) #listening for information
 
 #server side logic
-while True:
-    clientsocket, adress = sA.accept()
-    sB.connect((socket.gethostname(), 9000))
-    print(f"Server had connected to {adress} successfully!")
-    """
-    msg = "Welcome to the server!"
-    header_with_msg = f'{len(msg):<{HEADERSIZE}}' + msg
+clientsocket, adress = sA.accept()
+print(f"Server had connected to {adress} successfully!")
     
-    clientsocket.send(bytes(header_with_msg, "utf-8"))
-    """
+#connecting socket
+sB.connect((socket.gethostname(), 5000))
 
-#client side logic
-"""
-while True:
-    decoded_msg = ''
-    new_msg = True
+def sending():
     while True:
-        msg = s.recv(50)
-        if new_msg:
-            print("Incomming message length: ", msg[:HEADERSIZE])
-            message_length = int(msg[:HEADERSIZE])
-            new_msg = False
+        user = input(r'')
+        clientsocket.send(bytes(user, "utf-8"))
+        continue
 
-        decoded_msg += msg.decode("utf-8")
-        
-        if len(decoded_msg)-HEADERSIZE == message_length:
-            print("Full Message received")
-            print(decoded_msg[HEADERSIZE:])
-            new_msg = True
-"""
+def receiving():
+    while True:
+        msg = sB.recv(2048)
+        decode = msg.decode("utf-8")
+        print(decode)
+        continue
+
+t1 = threading.Thread(target=sending)
+t2 = threading.Thread(target=receiving)
+
+t1.start()
+t2.start()
