3e5c4847a675bca2a361d9fe35c2fb8e99526aae
matchallenges <matchallenges04@gmail.com>
2021-04-11 21:11:01 -0400

    We did a program from a book

---
+++ age.py
@@ -0,0 +1,10 @@
+age = 30
+
+if age <= 5 and age >= 0:
+    print(" Wow so young")
+elif age <= 10 and age >= 5:
+    print(" Becoming older")
+elif age <= 20 and age >= 10:
+    print(" Adult")
+elif age >= 20:
+    print(" Almost dead")
